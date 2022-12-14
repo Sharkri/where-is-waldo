d8d7aed430fc08c94c36c14fcaded4d4a391b3b6
Sharkri <sharkdude955@gmail.com>
2022-12-13 23:53:12 -0600

    Add leaderboard mapping

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { PropTypes } from "prop-types";
+import LeaderboardSubmission from "./LeaderboardSubmission";
 
-function Leaderboard() {
-  return <div className="leaderboard">leaderboard page....</div>;
+function Leaderboard({ leaderboard }) {
+  return (
+    <table>
+      <tbody className="leaderboard">
+        {leaderboard.map((submission) => (
+          <LeaderboardSubmission submission={submission} key={submission.id} />
+        ))}
+      </tbody>
+    </table>
+  );
 }
+
+Leaderboard.propTypes = {
+  leaderboard: PropTypes.arrayOf(
+    PropTypes.shape({
+      place: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      startTime: PropTypes.instanceOf(Date).isRequired,
+      endTime: PropTypes.instanceOf(Date).isRequired,
+      dateSubmitted: PropTypes.instanceOf(Date).isRequired,
+    }).isRequired
+  ).isRequired,
+};
 
 export default Leaderboard;
