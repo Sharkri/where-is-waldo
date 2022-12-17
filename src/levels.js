3a36cff598640b96da04e37428b2fec0c8f7475b
Sharkri <sharkdude955@gmail.com>
2022-12-16 20:54:12 -0600

    Add new character

--- src/levels.js
+++ src/levels.js
@@ -11,6 +11,12 @@ const levels = [
           "https://www.giantbomb.com/a/uploads/scale_medium/9/95666/1907632-new_super_mario_bros._wii_toad.png",
         id: 0,
       },
+      {
+        name: "Blinky",
+        photo:
+          "https://www.giantbomb.com/a/uploads/scale_small/8/87790/2469740-blinky.png",
+        id: 1,
+      },
     ],
     id: 0,
   },
