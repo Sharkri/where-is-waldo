b3145747ee8cbce659f4e0f07996653880a98f34
Sharkri <sharkdude955@gmail.com>
2022-12-16 18:31:05 -0600

    Ignore eslint errors

--- src/components/tests/PhotoLevelInstructions.test.js
+++ src/components/tests/PhotoLevelInstructions.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
