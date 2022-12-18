53170b4901e18fd447f46ce88b735514f7b8af3b
Sharkri <sharkdude955@gmail.com>
2022-12-18 16:36:23 -0600

    Get actual coordinates

--- src/components/GameLevel.js
+++ src/components/GameLevel.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Header from "./Header";
 import getLevelById from "../helper/getLevelById";
@@ -19,16 +19,39 @@ function GameLevel() {
   const [coords, setCoords] = useState({ x: 0, y: 0 });
   const [containerSize, setContainerSize] = useState(null);
 
+  // STORE COORDS IN FIREBASE LATER
+  const [originalX, originalY] = [623, 742];
+
   const onStart = () => {
     setIsStarted(true);
     setStartTime(Date.now());
     setInterval(() => setCurrentTime(Date.now()), 1);
   };
 
+  // Work out the x and y coord as a percentage of the width.
+  const getActualCoords = () => {
+    const originalImg = { height: 822, width: 640 };
+
+    // Gets original image's x and y percentage
+    const percentage = {
+      x: (originalX / originalImg.width) * 100,
+      y: (originalY / originalImg.height) * 100,
+    };
+    // Convert x and y to decimal and then times by width/height
+    const x = (percentage.x / 100) * containerSize.width;
+    const y = (percentage.y / 100) * containerSize.height;
+    console.log(x, y);
+  };
+
+  useEffect(() => {
+    if (containerSize) console.log(getActualCoords());
+  }, containerSize);
+
   const handleImageClick = (e) => {
     if (!isStarted) return;
     const image = e.target;
     setContainerSize({ height: image.scrollHeight, width: image.scrollWidth });
+
     setIsDropdownOpen(!isDropdownOpen);
     const rect = image.getBoundingClientRect();
     setCoords({
@@ -88,3 +111,4 @@ function GameLevel() {
 }
 
 export default GameLevel;
+// http://localhost:3000/levels/0?x=1238&y=1472
