afd2c9cd598cffbf0cbcf53d8a66f38664c8ac11
Sharkri <sharkdude955@gmail.com>
2022-12-18 20:09:48 -0600

    Fix getLevelById tests

--- src/helper/tests/getLevelById.test.js
+++ src/helper/tests/getLevelById.test.js
@@ -1,13 +1,26 @@
 import getLevelById from "../getLevelById";
 
-jest.mock("../../levels.js", () => [
-  { id: 0, name: "first one" },
-  { id: 1, name: "number 2" },
-  { id: 2, name: "3rd one" },
-]);
+jest.mock(
+  "../levels.js",
+  () => () =>
+    Promise.resolve([
+      { id: 0, name: "first one", photo: "xd.png" },
+      { id: 1, name: "number 2" },
+      { id: 2, name: "3rd one" },
+    ])
+);
 
-it("should get level by id", () => {
-  expect(getLevelById(0).name).toBe("first one");
-  expect(getLevelById(1).name).toBe("number 2");
-  expect(getLevelById(2).name).toBe("3rd one");
+jest.mock("../../backend.js", () => ({
+  getImage: (src) => Promise.resolve(`test${src}`),
+}));
+
+it("should get level by id", async () => {
+  expect((await getLevelById(0)).name).toBe("first one");
+  expect((await getLevelById(1)).name).toBe("number 2");
+  expect((await getLevelById(2)).name).toBe("3rd one");
+});
+
+it("should get image from backend.js", async () => {
+  const level = await getLevelById(0);
+  expect(level.photo).toBe("testxd.png");
 });
