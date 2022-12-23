83fd25c7c4dace38aad36dee6f09af27262f0c75
Sharkri <sharkdude955@gmail.com>
2022-12-23 16:16:29 -0600

    Add submit to leaderboard functionality

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { PropTypes } from "prop-types";
 import "../css/Dropdown.css";
 import formatTimeDuration from "../helper/formatTimeDuration";
+import submitToLeaderboard from "../helper/submitToLeaderboard";
 
-function GameEnd({ start, end }) {
+function GameEnd({ start, end, level }) {
+  const [name, setName] = useState("");
+
   return (
     <div>
       <p>You finished in {formatTimeDuration(start, end)}!</p>
-      <form action="" onSubmit={(e) => e.preventDefault()}>
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          const newLevel = { ...level };
+          // if leaderboard doesn't exist yet
+          if (!newLevel.leaderboard) newLevel.leaderboard = [];
+          // push new entry to leaderboard
+          newLevel.leaderboard.push({ name, start, end });
+
+          submitToLeaderboard(level.id, newLevel);
+        }}
+      >
         <label htmlFor="name">
           Name:
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
 
         <button type="submit">Submit Score</button>
@@ -22,6 +42,9 @@ function GameEnd({ start, end }) {
 GameEnd.propTypes = {
   start: PropTypes.instanceOf(Date).isRequired,
   end: PropTypes.instanceOf(Date).isRequired,
+  level: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  }).isRequired,
 };
 
 export default GameEnd;
--- src/components/tests/GameEnd.test.js
+++ src/components/tests/GameEnd.test.js
@@ -3,21 +3,43 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import GameEnd from "../GameEnd";
+import submitToLeaderboard from "../../helper/submitToLeaderboard";
 
 jest.mock("../../helper/formatTimeDuration.js", () =>
   jest.fn((start, end) => end - start)
 );
 
+jest.mock("../../helper/submitToLeaderboard.js", () => jest.fn());
+
+const mockLevel = {
+  id: "foobar",
+  leaderboard: [{ name: "baz", start: 1000, end: 2000 }],
+};
+
 it("should show time taken", () => {
-  render(<GameEnd start={new Date(250)} end={new Date(1000)} />);
+  render(
+    <GameEnd start={new Date(250)} end={new Date(1000)} level={mockLevel} />
+  );
   expect(screen.getByText("You finished in 750!"));
 });
 
 it("renders correct input values and submits properly", () => {
-  render(<GameEnd start={new Date(250)} end={new Date(1000)} />);
-  userEvent.type(screen.getByRole("textbox"), "hello");
-  expect(screen.getByRole("textbox")).toHaveValue("hello");
+  render(
+    <GameEnd start={new Date(250)} end={new Date(1000)} level={mockLevel} />
+  );
+  userEvent.type(screen.getByRole("textbox"), "jimmy");
+  expect(screen.getByRole("textbox")).toHaveValue("jimmy");
 
-  // test later...
   userEvent.click(screen.getByRole("button", { name: /Submit Score/i }));
+  // should be called with id and the new value
+  expect(submitToLeaderboard).toHaveBeenCalledWith(
+    "foobar",
+    expect.objectContaining({
+      id: "foobar",
+      leaderboard: [
+        { name: "baz", start: 1000, end: 2000 },
+        { name: "jimmy", start: new Date(250), end: new Date(1000) },
+      ],
+    })
+  );
 });
